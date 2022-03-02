983e2a37f931e12028bc48305920ec14ead6ae31
Simon Prickett <simon@redislabs.com>
2022-03-02 17:39:01 +0000

    Updated person model to be JSON and have address, skills, default and optional values.

--- person.py
+++ person.py
@@ -1,7 +1,21 @@
-from redis_om import (Field, HashModel)
+from redis_om import (EmbeddedJsonModel, Field, JsonModel)
 from pydantic import PositiveInt
+from typing import Optional, List
 
-class Person(HashModel):
+class Address(EmbeddedJsonModel):
+    street_number: PositiveInt = Field(index=True)
+
+    # Unit isn't in all addresses, so let's make it optional...
+    unit: Optional[str] = Field(index=True)
+    street_name: str = Field(index=True)
+    city: str = Field(index=True)
+    state: Optional[str] = Field(index=True)
+    postal_code: str = Field(index=True)
+
+    # Provide a default value if none supplied...
+    country: str = Field(index=True, default="United Kingdom")
+
+class Person(JsonModel):
     # Indexed for exact text matching
     first_name: str = Field(index=True)
     last_name: str = Field(index=True)
@@ -9,8 +23,10 @@ class Person(HashModel):
     # Indexed for numeric matching
     age: PositiveInt = Field(index=True)
 
+    # Use an embedded sub-model
+    address: Address
+
+    skills: List[str] = Field(index=True)
+
     # Indexed for full text search
     personal_statement: str = Field(index=True, full_text_search=True)
-
-    # Geolocation (not yet supported in Redis OM Python
-    # TODO
